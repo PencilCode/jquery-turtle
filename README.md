d35720050119c26ceb283ffec14e47993bc48b46
David Bau <david.bau@gmail.com>
2013-04-26 08:52:53 -0400

    version 2.0.0

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 jQuery-turtle
 =============
 
-version 2.0
+version 2.0.0
 
 jQuery-turtle is a jQuery plugin for turtle graphics.
 
--- jquery-turtle.js
+++ jquery-turtle.js
@@ -4,7 +4,7 @@
 jQuery-turtle
 =============
 
-version 2.0
+version 2.0.0
 
 jQuery-turtle is a jQuery plugin for turtle graphics.
 
