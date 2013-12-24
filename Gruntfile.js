c28fdd7f370af67ed853fa787ff06ac6e011b91e
David Bau <david.bau@gmail.com>
2013-12-24 12:42:17 +0000

    Added bowercopy to manage test dependencies.

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,21 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON("package.json"),
+    bowercopy: {
+      options: {
+        clean: true
+      },
+      test: {
+        options: {
+          destPrefix: "test/lib"
+        },
+        files: {
+          "qunit.js" : "qunit/qunit/qunit.js",
+          "qunit.css" : "qunit/qunit/qunit.css",
+          "jquery.js" : "jquery/jquery.js"
+        }
+      }
+    },
     uglify: {
       all: {
         files: {
@@ -27,6 +42,7 @@ module.exports = function(grunt) {
     }
   });
 
+  grunt.loadNpmTasks('grunt-bowercopy');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-qunit');
   grunt.loadNpmTasks('grunt-release');
